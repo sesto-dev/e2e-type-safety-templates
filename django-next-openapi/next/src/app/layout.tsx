4697b816e02c5ef2aa58af035dfee442a574b81a
Amirhossein Mohammadi <amohammadi@kcrnd.co>
2025-10-15 11:11:52 +0330

    add unauthenticated redirect back

--- django-next-openapi/next/src/app/layout.tsx
+++ django-next-openapi/next/src/app/layout.tsx
@@ -1,26 +1,25 @@
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic'
 
-import type { Metadata, Viewport } from "next";
-import { redirect } from "next/navigation";
-import { Inter } from "next/font/google";
+import type { Metadata, Viewport } from 'next'
+import { redirect } from 'next/navigation'
+import { Inter } from 'next/font/google'
 
-import ThemeProvider from "~/components/shared/theme-provider";
-import { Toaster } from "~/components/ui/sonner";
-import { siteConfig } from "~/config/site";
-import getCurrentUser from "~/lib/server/current-user";
+import ThemeProvider from '~/components/shared/theme-provider'
+import { Toaster } from '~/components/ui/sonner'
+import { siteConfig } from '~/config/site'
+import getCurrentUser from '~/lib/server/current-user'
 
-import "./globals.css";
-
+import './globals.css'
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter", // exposes a CSS variable
-});
+  subsets: ['latin'],
+  variable: '--font-inter', // exposes a CSS variable
+})
 
-const APP_NAME = siteConfig().name;
-const APP_DEFAULT_TITLE = siteConfig().name;
-const APP_TITLE_TEMPLATE = `%s - ${siteConfig().name}`;
-const APP_DESCRIPTION = siteConfig().name;
+const APP_NAME = siteConfig().name
+const APP_DEFAULT_TITLE = siteConfig().name
+const APP_TITLE_TEMPLATE = `%s - ${siteConfig().name}`
+const APP_DESCRIPTION = siteConfig().name
 
 export const metadata: Metadata = {
   applicationName: APP_NAME,
@@ -31,14 +30,14 @@ export const metadata: Metadata = {
   description: APP_DESCRIPTION,
   appleWebApp: {
     capable: true,
-    statusBarStyle: "default",
+    statusBarStyle: 'default',
     title: APP_DEFAULT_TITLE,
   },
   formatDetection: {
     telephone: false,
   },
   openGraph: {
-    type: "website",
+    type: 'website',
     siteName: APP_NAME,
     title: {
       default: APP_DEFAULT_TITLE,
@@ -47,28 +46,29 @@ export const metadata: Metadata = {
     description: APP_DESCRIPTION,
   },
   twitter: {
-    card: "summary",
+    card: 'summary',
     title: {
       default: APP_DEFAULT_TITLE,
       template: APP_TITLE_TEMPLATE,
     },
     description: APP_DESCRIPTION,
   },
-};
+}
 
 export const viewport = {
   width: 1,
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
   ],
-};
+}
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
+  const user = await getCurrentUser()
 
   return (
     <html lang="en" className={inter.variable} suppressHydrationWarning={true}>
@@ -82,5 +82,5 @@ export default async function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
